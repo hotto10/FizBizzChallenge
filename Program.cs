cb3ff7aa9a11c5cf3ecdf6ac5705f49552442fe3
heather otto <heatherotto32@yahoo.com>
2022-11-14 14:25:10 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FizzBizzChallenge
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a whole number to see if divisible by 3 or 5");
+            var number = int.Parse(Console.ReadLine());
+            GetFuzzBuzz(number);
+        }
+
+        public static string GetFuzzBuzz(int number)
+        {
+            var word = "";
+
+            if (number % 3 == 0)
+            {
+                word = "fizz";
+            }
+
+            if (number % 5 == 0)
+            {
+                word += "buzz";
+            }
+
+            Console.WriteLine(word);
+            return word;
+
+        }
+
+    }
+}
